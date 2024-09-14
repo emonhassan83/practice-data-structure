33d38a18991a32e798892720f500d3f15f7280be
Emon <emonhasan7650@gmail.com>
2024-09-14 20:13:19 +0600

    queue implement & stl implemention done

---
+++ Week-04. Stack_&_Queue/2. queue_implement_&_stl/implement_queue_using_stack.cpp
@@ -0,0 +1,58 @@
+//* https://leetcode.com/problems/implement-queue-using-stacks/
+
+/* 
+class MyQueue {
+public:
+    stack<int> st;
+    MyQueue() {}
+
+    void push(int x) { st.push(x); }
+
+    int pop() {
+        stack<int> newSt;
+        int last;
+
+        while (!st.empty()) {
+            int k = st.top();
+            st.pop();
+
+            if (st.empty()) {
+                last = k;
+                break;
+            }
+            newSt.push(k);
+        }
+
+        while (!newSt.empty()) {
+            st.push(newSt.top());
+            newSt.pop();
+        }
+        return last;
+    }
+
+    int peek() {
+        stack<int> newSt;
+        int last;
+
+        while (!st.empty()) {
+            int k = st.top();
+            st.pop();
+
+            if (st.empty()) {
+                last = k;
+            }
+            newSt.push(k);
+        }
+
+        while (!newSt.empty()) {
+            st.push(newSt.top());
+            newSt.pop();
+        }
+        return last;
+    }
+
+    bool empty() {
+        return st.empty();
+    }
+};
+ */
